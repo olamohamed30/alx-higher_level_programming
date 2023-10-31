233357680d07e108ad7e6d0494ec0e2d7c927dc9
ola mohamed <olamohamedola276@gmail.com>
2023-10-31 11:09:39 -0700

    ola

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a ^ b
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / (a ** abs(b))
+    else:
+        res = 1
+        for _ in range(b):
+            res *= a
+            return res
