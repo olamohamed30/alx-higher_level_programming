458463b7697c340e4ba6df6c0c6dabb953ed6eb5
ola mohamed <olamohamedola276@gmail.com>
2023-10-31 09:57:22 -0700

    ola

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-lds = str(number)[-1]
-ld = int(lds) if number >= 0 else -int(lds)
-print(ld)
+    ld = abs(number) % 10
+    print("{}".format(ld), end="")
+    return ld
