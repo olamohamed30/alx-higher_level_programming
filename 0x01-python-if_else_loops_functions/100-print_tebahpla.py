9161f8328017b047691cc87cd0d949c50ecf5467
ola mohamed <olamohamedola276@gmail.com>
2023-10-31 14:48:22 -0700

    ola

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 == 0:
-        print(chr(i), end="")
-    else:
-        print(chr(i - 32), end="")
+print("{}".format("".join([chr(i - 32) if i % 2 != 0 else chr(i) for i in range(122, 96, -1)])))
