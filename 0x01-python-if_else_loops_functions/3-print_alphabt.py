d8e71c917422ae7128cfa61c42050f3b6979896f
ola mohamed <olamohamedola276@gmail.com>
2023-10-31 07:09:44 -0700

    ola

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113: 
-        print(chr(i), end="")
+print("".join(chr(ind) for ind in range(97, 123) if ind != 101 and ind != 113))
