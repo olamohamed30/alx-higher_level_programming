9c818ec6fef680938cf09abf1249f3149057d014
ola mohamed <olamohamedola276@gmail.com>
2023-11-02 06:35:56 -0700

    ola

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
-res = add(a, b)
-
-print("{} + {} = {}".format(a, b, res))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    res = add(a, b)
+    print("{} + {} = {}".format(a, b, res))
