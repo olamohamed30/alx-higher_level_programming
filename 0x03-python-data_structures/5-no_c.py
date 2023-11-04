0d1b95631c7ac9a40b6ec87ed91c9dad326d2da3
ola mohamed <olamohamedola276@gmail.com>
2023-11-04 10:18:19 -0700

    ola

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    news = ''
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            news += i
+    news = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            news += my_string[i]
     return news
