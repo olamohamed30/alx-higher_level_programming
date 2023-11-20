5e7a71a95c068f1dd72edd3525e123112d8cb12d
ola mohamed <olamohamedola276@gmail.com>
2023-11-20 04:05:11 -0800

    ola

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
@@ -8,3 +9,4 @@ def safe_print_list(my_list=[], x=0):
             break
     print()
     return count
+
