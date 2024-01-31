54d950ab119c3de2d0210d5dbcd1209803e00ca2
ola mohamed <olamohamedola276@gmail.com>
2024-01-31 11:55:31 -0800

    ola

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""defining my rec class """
+
+class Rectangle:
+    """ emty cass"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+class Rectangle:
+    """Represents a rec withw ,h"""
+
+    def __init__(self, width=0, height=0):
+        """optional width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setsh of the rectanglenteger."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrif the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets ctangle, ensuring it's a valid integer."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
