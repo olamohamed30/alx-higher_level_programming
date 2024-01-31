33e2b99b5ceda54f0e2cb44f71c89f9ede0713a7
ola mohamed <olamohamedola276@gmail.com>
2024-01-31 12:08:51 -0800

    ola

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+defines a class rec
+"""
+class Rectangle:
+    """Represents a rec withw ,h"""
+
+    def __init__(self, width=0, height=0):
+        """optional width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setsh of the rectanglenteger."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrif the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets ctangle, ensuring it's a valid integer."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the a the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns the perimet."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
